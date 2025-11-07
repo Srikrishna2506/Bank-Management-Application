29c4e5927943cdb31cb6e97b8d4f1b7fc62ddce8
Srikrishna Laxetti <149474612+Srikrishna2506@users.noreply.github.com>
2025-11-07 14:43:37 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,17 @@
+
+MAX_LOGIN_ATTEMPTS = 5
+
+#database configuration
+HOST = "localhost"
+USER = "root"
+PASSWORD = "kittu"
+DATABASE = "flm_bms"
+
+# MAIL CREDENTIALS
+SENDERMAIL = 'kittuak25@gmail.com'
+MAILPASS = 'oqmwhmfchipwetub'
+
+# BANNER PATH
+
+
+
